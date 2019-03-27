dc9ef8acd0130ce1c5da52b21d11a1509cb8e70f
pwei12 <wpwei-16@yahoo.com>
2019-03-27 17:48:31 +0800

    Install cross-env; Add env variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 const port = process.env.PORT || 8080;
-const mongodbUri = "mongodb://localhost/grey-leaf";
+const mongodbUri = process.env.MONGODB_URI || "mongodb://localhost/grey-leaf";
 
 mongoose.connect(mongodbUri, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
 const db = mongoose.connection;
@@ -13,6 +13,10 @@ db.on('error', err => {
 db.once('open', () => {
   console.log("Connected to database");
   app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`)
+    if (process.env.NODE_ENV === "production") {
+      console.log(`Server is running on Heroku with port number ${port}`);
+    } else {
+      console.log(`Server is running on http://localhost:${port}`);
+    }
     });
 });
