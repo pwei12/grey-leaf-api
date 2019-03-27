9fb95c2c9ae949b2af093bd7352e3583a913da97
pwei12 <wpwei-16@yahoo.com>
2019-03-27 10:54:23 +0800

    Change ACORS middleware into async

--- app.js
+++ app.js
@@ -3,21 +3,17 @@ const app = express();
 
 const products = require("./routes/products");
 const users = require("./routes/users");
-// const login = require("./routes/login");
-// const index = require("./routes/index");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+app.use(async(req, res, next) => {
+  await res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   // res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   // res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  });
+  next();
+});
 
 app.use("/api/v1/products", products);
 app.use("/api/v1/users", users);
-// app.use("/login", login);
-// app.use("/", index);
 
 module.exports = app;
