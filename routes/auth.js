4d46c7fe7615900c76cbe74878528bdabfac7ff8
pwei12 <wpwei-16@yahoo.com>
2019-04-01 07:14:16 +0800

    Update auth controller

--- routes/auth.js
+++ routes/auth.js
@@ -18,11 +18,13 @@ const saltRounds = 10;
 
 router.route("/signup").post(async (req, res) => {
   try {
-      const {name, email,  password, confirmedPassword} = req.body;
-      if(!(name && email && password && confirmedPassword)) return res.status(400).json("Incomplete field.")
+    const { name, email, password, confirmedPassword } = req.body;
+    if (!(name && email && password && confirmedPassword))
+      return res.status(400).json("Incomplete field.");
     const user = await isUserAlreadyExist(name, email);
     if (user) return res.status(403).json("User already exist.");
-    if (password !== confirmedPassword) return res.status(400).json("Please confirm your password again.");
+    if (password !== confirmedPassword)
+      return res.status(400).json("Please confirm your password again.");
     const hash = await bcrypt.hash(password, saltRounds);
     const userData = {
       name: req.body.name,
@@ -32,7 +34,7 @@ router.route("/signup").post(async (req, res) => {
     const newUser = await new User(userData);
     await User.init();
     await newUser.save();
-    return res.status(202).json({id: newUser.id});
+    return res.status(202).json({ id: newUser.id });
   } catch (err) {
     return res.status(500).json(err);
   }
@@ -43,17 +45,13 @@ router.route("/login").post(async (req, res) => {
     const { name, email, password } = req.body;
     const user = await isUserAlreadyExist(name, email);
     if (!user) return res.status(404).json("Please sign up.");
-    const verifiedPassword = await bcrypt.compare(
-      password,
-      user.password
-    );
-    if (!verifiedPassword)
-      return res.status(401).json("Wrong password.");
+    const verifiedPassword = await bcrypt.compare(password, user.password);
+    if (!verifiedPassword) return res.status(401).json("Wrong password.");
     const token = await jwt.sign(req.body, "shhhhh");
     return res
       .cookie("token", token)
       .status(200)
-      .json(user.cart);
+      .json(user.id);
   } catch (err) {
     return res.status(500).json(err);
   }
