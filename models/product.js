2cd815f39f8216d40c43867e99c3da018849c114
pwei12 <wpwei-16@yahoo.com>
2019-05-01 12:19:17 +0800

    Remove alias and inCart; change bestSeller to be string type

--- models/product.js
+++ models/product.js
@@ -1,37 +1,29 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    d: {
-        type: String,
-        required: true,
-        trim: true,
-        alias: "description"
-    },
-    url: {
-        type: String,
-        required: true,
-        alias: "imageUrl"
-    },
-    inC: {
-        type: Boolean,
-        required: true,
-        alias: "inCart"
-    },
-    bs: {
-        type: Boolean,
-        required: true,
-        alias: "bestSeller"
-    }
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  bestSeller: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model("Product", productSchema);
